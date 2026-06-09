4682a99311dd9bb61310a389cb823a4c73075247
Ariket Gupta <ariketgupta@gmail.com>
2026-06-09 22:48:49 +0530

    Add user model

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const Attendance = require("../models/attendance");
 const Subscription = require("../models/subscription");
 
