989883bce3d960b334d81dcf286b75d00b59d2f0
Ariket Gupta <ariketgupta@gmail.com>
2026-06-09 22:23:42 +0530

    small change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const dashboardRoutes = require("./backend/routes/dashboardRoutes");
 const adminRoutes = require("./backend/routes/adminRoutes");
 const path = require("path");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
